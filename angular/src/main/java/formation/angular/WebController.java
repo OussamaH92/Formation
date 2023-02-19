2eaac1e2ffed823d3870996664e42e06a1891c32
OussamaH92 <oussamahaddad92230@hotmail.com>
2023-02-19 23:50:18 +0100

    Ajout composant navBar

--- angular/src/main/java/formation/angular/WebController.java
+++ angular/src/main/java/formation/angular/WebController.java
@@ -1,6 +1,7 @@
 package formation.angular;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,6 +13,14 @@ public class WebController {
     public ModelAndView entry(){
 
         return new ModelAndView("index");
+
+    }
+
+    @GetMapping("/json")
+    @CrossOrigin(origins = "http://localhost:4200/")
+    public String getData(){
+
+        return "{'a':'bb','b':'aa'}";
 
     }
 }
